f986d6133ccf7603331f9bff16501ebb3edb1fe0
alanmartinsedna <alan.martins@edna.center>
2026-03-23 10:43:39 -0300

    Criado novo arquivo para relotorio dinamico diagnostico

---
+++ diagnostic-dinamic-report.py
@@ -0,0 +1,7 @@
+from reportlab.platypus import (
+    BaseDocTemplate, Frame, PageTemplate,
+    Paragraph, Table, TableStyle, Spacer
+)
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
